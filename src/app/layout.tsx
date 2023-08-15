a18882ac38f9639df3da215750ea2bfcd671e6f6
Michaela <michaela.rudolfova@seznam.cz>
2023-08-15 14:43:47 +0200

    add navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,9 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
-import { Inter } from "next/font/google";
 import React from "react";
 
-import { Locale } from "@/config/intl";
-
-import AllProvider from "@/components/AllProvider";
+import RootLayoutComponent from "@/components/RootLayoutComponent";
 
 export const metadata: Metadata = {
   icons: {
@@ -21,20 +18,10 @@ export const metadata: Metadata = {
   },
 };
 
-const inter = Inter({ subsets: ["latin"] });
-
 type Props = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  const locale = Locale.EN;
-
-  return (
-    <html lang={locale}>
-      <body className={inter.className}>
-        <AllProvider>{children}</AllProvider>
-      </body>
-    </html>
-  );
+  return <RootLayoutComponent>{children}</RootLayoutComponent>;
 }
