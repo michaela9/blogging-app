05b142a0a97a1b4430e3746ad963df7f7ecd9a88
Michaela <michaela.rudolfova@seznam.cz>
2023-09-03 23:40:20 +0200

    set token empty string

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -74,7 +74,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const logout = () => {
     Cookies.remove("token");
     Cookies.remove("expirationTime");
-    setToken(null);
+    setToken("");
     setIsLoggedIn(false);
     router.push(AppUrl.home);
   };
